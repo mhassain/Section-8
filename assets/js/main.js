97908d2b8262b079834d4ce171caf058566f5396
Mahade hasan <mahadehasan88@gmail.com>
2018-11-19 15:13:01 +0600

    section8

---
+++ assets/js/main.js
@@ -0,0 +1,18 @@
+(function ($){
+	"use strict";
+
+	jQuery(document).ready(function($){
+
+		$(".play-btn").magnificPopup({
+			type: "video",
+		});
+	
+
+	});
+
+
+	jQuery(window).load(function(){
+
+	})	
+	
+}(jQuery));
